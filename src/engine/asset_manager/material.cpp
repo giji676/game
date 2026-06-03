ce0508a1feecb734feba0c234bc31a2943b56281
giji676 <tvtengoxachidze@gmail.com>
2026-06-03 21:13:59 +0100

    shader misuse in material.bind and renderer.render cleared up

--- src/engine/asset_manager/material.cpp
+++ src/engine/asset_manager/material.cpp
@@ -3,28 +3,8 @@
 #include "material.h"
 
 void Material::bind() const {
-    // shader->use();
-
-    unsigned int diffuseNr = 1;
-    unsigned int specularNr = 1;
-
     for (unsigned int i = 0; i < textures.size(); i++) {
         glActiveTexture(GL_TEXTURE0 + i);
-
-        std::string type = textures[i]->type;
-        std::string number;
-
-        if (type == "diffuseMap")
-            number = std::to_string(diffuseNr++);
-
-        else if (type == "specularMap")
-            number = std::to_string(specularNr++);
-
-        shader->setInt(type + number, i);
-
         glBindTexture(GL_TEXTURE_2D, textures[i]->id);
     }
-
-    shader->setVec3("diffuseFallback", diffuseFallback);
-    shader->setVec3("specularFallback", specularFallback);
 }
--- src/engine/renderer/renderer.cpp
+++ src/engine/renderer/renderer.cpp
@@ -12,17 +12,26 @@ void Renderer::submit(const Object& object,
 }
 
 void Renderer::render(const glm::mat4& view,
-                      const glm::mat4& projection) {
+                      const glm::mat4& projection)
+{
     for (const RenderCommand& cmd : queue) {
         Shader* shader = cmd.material->shader;
 
         shader->use();
 
-        cmd.material->bind();
-
         shader->setMat4("model", cmd.model);
         shader->setMat4("view", view);
         shader->setMat4("projection", projection);
+
+        cmd.material->bind();
+
+        for (unsigned int i = 0; i < cmd.material->textures.size(); i++) {
+            const std::string& type = cmd.material->textures[i]->type;
+            shader->setInt(type + std::to_string(i + 1), i);
+        }
+
+        shader->setVec3("diffuseFallback", cmd.material->diffuseFallback);
+        shader->setVec3("specularFallback", cmd.material->specularFallback);
 
         cmd.mesh->draw();
     }
