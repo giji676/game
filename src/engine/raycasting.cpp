aecb3b0e58ec58025fe7fc9d89d9ba5c44747d43
giji676 <tvtengoxachidze@gmail.com>
2026-06-07 01:00:45 +0100

    added ray-triangle intersection (very slow for now)

--- src/engine/raycasting.cpp
+++ src/engine/raycasting.cpp
@@ -49,15 +49,59 @@ void Raycasting::checkIntersect(
 
         float distance;
 
-        if (testRaySphereIntersection(
+        if (testSphereIntersection(
             rayOrigin,
             rayDirection,
             center,
             radius,
             distance))
         {
-            if (distance < bestHit.distance) {
-                bestHit.distance = distance;
+            glm::mat4 invWorld = glm::inverse(world);
+
+            glm::vec3 localOrigin =
+                glm::vec3(invWorld * glm::vec4(rayOrigin, 1.0f));
+
+            glm::vec3 localDirection =
+                glm::normalize(
+                    glm::vec3(invWorld * glm::vec4(rayDirection, 0.0f))
+                );
+
+            float closestTriDist = FLT_MAX;
+            bool hitTriangle = false;
+
+            for (const SubMesh& part : obj.model->getParts()) {
+                const auto& verts = part.mesh.vertices;
+                const auto& indices = part.mesh.indices;
+
+                for (size_t i = 0; i < indices.size(); i += 3) {
+
+                    triangle3 tri{
+                        verts[indices[i + 0]].Position,
+                        verts[indices[i + 1]].Position,
+                        verts[indices[i + 2]].Position
+                    };
+
+                    auto hitPos =
+                        testTriangleIntersection(
+                            localOrigin,
+                            localDirection,
+                            tri);
+
+                    if (!hitPos)
+                        continue;
+
+                    float dist =
+                        glm::length(*hitPos - localOrigin);
+
+                    if (dist < closestTriDist) {
+                        closestTriDist = dist;
+                        hitTriangle = true;
+                    }
+                }
+            }
+
+            if (hitTriangle && closestTriDist < bestHit.distance) {
+                bestHit.distance = closestTriDist;
                 bestHit.object = obj.getID();
             }
         }
@@ -68,7 +112,7 @@ void Raycasting::checkIntersect(
     }
 }
 
-bool Raycasting::testRaySphereIntersection(
+bool Raycasting::testSphereIntersection(
     const glm::vec3& rayOrigin,
     const glm::vec3& rayDirection,
     const glm::vec3& sphereCenter,
@@ -103,4 +147,44 @@ bool Raycasting::testRaySphereIntersection(
     }
 
     return false;
+}
+
+std::optional<glm::vec3> Raycasting::testTriangleIntersection(const glm::vec3& ray_origin,
+                                                  const glm::vec3 &ray_vector,
+                                                  const triangle3& triangle)
+{
+    // From https://en.wikipedia.org/wiki/M%C3%B6ller%E2%80%93Trumbore_intersection_algorithm
+    constexpr float epsilon = std::numeric_limits<float>::epsilon();
+
+    glm::vec3 edge1 = triangle.b - triangle.a;
+    glm::vec3 edge2 = triangle.c - triangle.a;
+
+    // Backface culling, assuming CCW-wound triangles.
+    const glm::vec3 normal = cross(edge1, edge2); // No need to normalize
+    if (dot(normal, ray_vector) > 0) return {};
+
+    glm::vec3 ray_cross_e2 = cross(ray_vector, edge2);
+    float det = dot(edge1, ray_cross_e2);
+
+    if (abs(det) < epsilon) return {}; // Ray is parallel to triangle
+
+    float inv_det = 1.0 / det;
+    glm::vec3 s = ray_origin - triangle.a;
+    float u = inv_det * dot(s, ray_cross_e2);
+
+    if (u < -epsilon || u - 1 > epsilon) return {}; // Ray passes outside edge2's bounds
+
+    glm::vec3 s_cross_e1 = cross(s, edge1);
+    float v = inv_det * dot(ray_vector, s_cross_e1);
+
+    if (v < -epsilon || u + v - 1 > epsilon) return {}; // Ray passes outside edge1's bounds
+
+    // The ray line intersects with the triangle.
+    // We compute t to find where on the ray the intersection is.
+    float t = inv_det * dot(edge2, s_cross_e1);
+
+    if (t > epsilon) // Ray intersection
+        return  glm::vec3(ray_origin + ray_vector * t);
+    else // This means that there is a line intersection but not a ray intersection.
+        return {};
 }
--- src/engine/raycasting.h
+++ src/engine/raycasting.h
@@ -1,9 +1,16 @@
 #pragma once
 
 #include <glm/glm.hpp>
+#include <optional>
 #include "defines.h"
 
 #define INVALID_OBJECT_ID 0
+
+typedef struct {
+    glm::vec3 a;
+    glm::vec3 b;
+    glm::vec3 c;
+} triangle3;
 
 struct RaycastHit {
     ObjectID object = INVALID_OBJECT_ID;
@@ -19,10 +26,14 @@ public:
         const glm::mat4& parent,
         RaycastHit& bestHit);
 
-    bool testRaySphereIntersection(
+    bool testSphereIntersection(
         const glm::vec3& rayOrigin,
         const glm::vec3& rayDirection,
         const glm::vec3& sphereCenter,
         float radius,
         float& intersectionDistance);
+
+    std::optional<glm::vec3> testTriangleIntersection(const glm::vec3& ray_origin,
+                                                      const glm::vec3 &ray_vector,
+                                                      const triangle3& triangle);
 };
